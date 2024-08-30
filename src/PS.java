62c1bedb8c12ac67909fc55143e7e279b5a08f51
divya <rayvid66@gmail.com>
2024-08-30 12:11:35 -0700

    this is commit from IST Timezone

--- src/PS.java
+++ src/PS.java
@@ -7,7 +7,9 @@ public void doThis()
 {
 		//Inheritance means you can acquire the properties of your parent class. Say you want to access
 		//methods, variables etc declared in this class into another class
-	System.out.println("I am here");	
+	System.out.println("I am here");
+	System.out.println("I am here2");
+	System.out.println("I am here3");
 	}
 @BeforeMethod
 public void beforeRun()
